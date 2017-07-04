4cebba9a51f96d852bb920778ea64dd2bdd77fbd
jmunsch <james.a.munsch@gmail.com>
2017-07-04 12:44:46 -0700

    osc

--- opensphericalcamera/test.py
+++ opensphericalcamera/test.py
@@ -3,16 +3,23 @@ from osc.theta import RicohThetaS
 thetas = RicohThetaS()
 thetas.state()
 thetas.info()
+response = thetas.listAll(entryCount=27)
+
+for img in response.get('results').get('entries'):
+    thetas.getImage(img.get('uri'))
+exit()
 import pdb;pdb.set_trace()
 # Capture image
 thetas.setCaptureMode( 'image' )
-response = thetas.takePicture()
 
-# Wait for the stitching to finish
-thetas.waitForProcessing(response['id'])
 
-# Copy image to computer
-thetas.getLatestImage()
+# response = thetas.takePicture()
+
+# # Wait for the stitching to finish
+# thetas.waitForProcessing(response['id'])
+
+# # Copy image to computer
+# thetas.getLatestImage()
 
 # Stream the livePreview video to disk
 # for 3 seconds
